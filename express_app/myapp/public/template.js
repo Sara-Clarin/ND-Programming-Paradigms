702ea37a7c301e863f07dedb01907d3d05d8fd80
Sara-Clarin <sclarin2@nd.edu>
2022-04-25 11:31:17 -0400

    List of template classes printing

--- express_app/myapp/public/template.js
+++ express_app/myapp/public/template.js
@@ -1,21 +1,25 @@
 
-if (10 == 10){
-    alert("working!");
-}
 
-let items = [
-        'Blue',
-        'Red',
-        'White',
-        'Green',
-        'Black',
-        'Orange'
-    ],
+let Tclasses = [
+        ['University Core Requirement', 'Engineering Design', 'Intro to Chemical Principles', 'Calculus 1', 'Moreau FYS'],['University Core Requirement', 
+    'Engineering Computing', 'Moreau FYS', 'Calculus 2', 'Physics 1'], 
+    ['University Core requirement', 'Discrete Mathematics', 'Fundamental Computing', 'Calculus 3', 'Physics 2'],
+    ['University Core Requirement', 'Logic Design', 'Systems Programming', 'Data Structures', 'Linear Algebra and Diffeq'],
+    ['University Core requirement', 'CSE Elective', 'CSE Elective', 'Computer Architecture', 'Tech Elective'],
+    ['University Core requirement','Theory of Computing', 'Programming Paradigms', 'Operating Systems', 'Probability and Statistics'],
+    ['CSE Elective', 'CSE ELective', 'Free Elective', 'Algorithms'],
+    ['University Core requirement','CSE Elective', 'CSE Elective', 'Ethical and Professional Issues' ],
+]   
     ul = document.createElement('ul');
 
-document.getElementByName('templateArea').appendChild(ul);
+//ument.getElementById('templateArea').appendChild('')
+const para = document.createElement('p');
+para.innerText = "Test text";
+document.body.appendChild(para);
 
-items.forEach(function (item) {
+document.getElementById('templateArea').appendChild(ul);
+
+Tclasses.forEach(function (item) {
     let li = document.createElement('li');
     ul.appendChild(li);
 
